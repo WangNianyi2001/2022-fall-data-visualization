e3ab23a0140e75dcacc7e41da01ada95f56a60a0
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-09-27 03:14:26 +0800

    Fix vite base

--- w2-dynamic-bar-chart/vite.config.mjs
+++ w2-dynamic-bar-chart/vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	root: '.',
+	base: '/2022-fall-data-visualization/w2-dynamic-bar-chart/dist/',
 	build: {
 		emptyOutDir: true,
 		outDir: 'dist',
