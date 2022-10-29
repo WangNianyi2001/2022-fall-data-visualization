83653a90a2f337ef53ab5d3989ce44a623465922
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-10-29 13:07:38 +0800

    Configure base URL

--- finale-pico-menu/vite.config.mjs
+++ finale-pico-menu/vite.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	root: 'src',
+	base: 'https://wangnianyi2001.github.io/2022-fall-data-visualization/finale-pico-menu/dist/',
 	build: {
+		emptyOutDir: true,
 		outDir: '../dist'
 	}
 });
